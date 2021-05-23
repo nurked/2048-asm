74899e9361eb3d96226f91a583d8342d98708b55
Ivan Roganov <ivanr@scientology.net>
2021-05-22 19:26:24 -0700

    Working out memstore

--- test.asm
+++ test.asm
@@ -12,7 +12,33 @@ segment  .data
 	resp	db	"a"
 
 segment	.bbs
-	stor	dw	0x0000000000000000f
+	stor	dw	0x0000000000000000f,0x0000000000000000f
+	;				byte addressing
+	; 00 00 00 00	[stor  ]	[stor+1]	[stor+2]	[stor+3]
+	; 00 01 00 00	[stor+4]	[stor+5]	[stor+6]	[stor+7]
+	; 00 01 00 00	[stor+8]	[stor+9]	[stor+a]	[stor+b]
+	; 00 00 00 00	[stor+c]	[stor+d]	[stor+e]	[stor+f]
+	;00 = nothing
+	;01 = 2
+	;02 = 4
+	;03 = 8
+	;04 = 16
+	;05 = 32
+	;06 = 64
+	;07 = 128
+	;08 = 256
+	;09 = 512
+	;0a = 1024
+	;0b = 2048
+	;0c = 4096
+	;0d = 8192
+	;0e = 16384
+	;0f = 32768
+	;01 = 65536 - maximum with the highest number is 2
+	;02 = 131072 - maximum with the highest number 4
+	;03 = 262144 - impossible
+
+
 section .text
 main:                                 
 	call 	readkey
