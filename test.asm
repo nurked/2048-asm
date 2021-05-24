08d745ca5b60a9b330a015a5f97026a74b97efea
Ivan Roganov <ivanr@scientology.net>
2021-05-23 19:20:16 -0700

    Updaing and commenting the display logic

--- test.asm
+++ test.asm
@@ -65,80 +65,40 @@ less:
 
 
 showoff:
-	xor		rdx, rdx
+	xor		rdx, rdx	;Clearing the registers we are going to use
 	xor		r8, r8
 	xor		r9, r9
+	xor		r14, r14
 
-	mov		r14, 00;
-	call	convert
-	mov		dl, al
+	mov		r14, 00		;Loading the first byte of data as a second param. (First param will be the format string)
+	call	convert		;converting digit to the letter
+	mov		dl, al		;rdx - second param
 
 	mov		r14, 01;
 	call	convert
-	mov		r8b, al
+	mov		r8b, al		;r8 - third param
 	
 	mov		r14, 02;
 	call	convert
-	mov		r9b, al
+	mov		r9b, al		;r9 - forth param
 
-	push 	rbp
+	push 	rbp			;save the stack pointer
 	mov 	rbp, rsp
 
-	mov		r14, 15
-	call	convert
-	push 	rax
+	mov		r13, 15		; run thru the rest of the data and push it via the stack
+	loop1:
 
-	mov		r14, 14
-	call	convert
-	push 	rax
+		mov		r14, r13
+		call	convert
+		push	rax
+		dec		r13
+		cmp		r13, 2
+		jne		loop1
 
-	mov		r14, 13
-	call	convert
-	push 	rax	
-
-	mov		r14, 12
-	call	convert
-	push 	rax
-
-	mov		r14, 11
-	call	convert
-	push 	rax		
-
-	mov		r14, 10
-	call	convert
-	push 	rax
-
-	mov		r14, 9
-	call	convert
-	push 	rax	
-
-	mov		r14, 8
-	call	convert
-	push 	rax	
-
-	mov		r14, 7
-	call	convert
-	push 	rax	
-
-	mov		r14, 6
-	call	convert
-	push 	rax	
-
-	mov		r14, 5
-	call	convert
-	push 	rax	
-
-	mov		r14, 4
-	call	convert
-	push 	rax	
-	mov		r14, 3
-	call	convert
-	push 	rax	
-
-	sub 	rsp, 32
-	lea 	rcx, [fmt]
-	call 	printf
-	leave
+	sub 	rsp, 32		;create shadow space
+	lea 	rcx, [fmt]	;Load the format string into memory
+	call 	printf		;call printf
+	leave				;we done for now, thank you!
 	ret
 
 readkey:
