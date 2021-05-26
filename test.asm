ec0c3f7677c5381e9089ce9af699b251ce669145
Ivan Roganov <ivanr@scientology.net>
2021-05-25 19:40:10 -0700

    Up,down, left and right are working now

--- test.asm
+++ test.asm
@@ -52,10 +52,26 @@ mainloop:
 
 	cmp		dword [resp], 's'
 	je		shutdown
+	
 	cmp		dword [resp], 'j'
-	jne		cont2
+	jne		cont_no_down
 	call	down
-cont2:
+cont_no_down:
+	cmp		dword [resp], 'k'
+	jne		cont_no_up
+	call	up
+cont_no_up:
+
+	cmp		dword [resp], 'h'
+	jne		cont_no_left
+	call	left
+cont_no_left:
+
+	cmp		dword [resp], 'l'
+	jne		cont_no_right
+	call	right
+cont_no_right:
+
 	call	showoff
 	jmp		mainloop	
 
@@ -159,13 +175,13 @@ down:
 	mov		[stor + 0xc], r10b
 	
 
-	mov		r10, [stor + 0xd];
+	mov		r10b, byte [stor + 0xd];
 	shl		r10, 8
-	add		r10, [stor + 0x9];
+	add		r10b, byte [stor + 0x9];
 	shl		r10, 8
-	add		r10, [stor + 0x5];
+	add		r10b, byte [stor + 0x5];
 	shl		r10, 8
-	add		r10, [stor + 0x1];
+	add		r10b, byte [stor + 0x1];
 
 	call 	shift
 	mov		[stor + 0x1], r10b
@@ -177,13 +193,13 @@ down:
 	mov		[stor + 0xd], r10b
 	
 
-	mov		r10, [stor + 0xe];
+	mov		r10b, byte [stor + 0xe];
 	shl		r10, 8
-	add		r10, [stor + 0xa];
+	add		r10b, byte [stor + 0xa];
 	shl		r10, 8
-	add		r10, [stor + 0x6];
+	add		r10b, byte [stor + 0x6];
 	shl		r10, 8
-	add		r10, [stor + 0x2];
+	add		r10b, byte [stor + 0x2];
 
 	call 	shift
 	mov		[stor + 0x2], r10b
@@ -195,13 +211,13 @@ down:
 	mov		[stor + 0xe], r10b
 	
 
-	mov		r10, [stor + 0xf];
+	mov		r10b, byte [stor + 0xf];
 	shl		r10, 8
-	add		r10, [stor + 0xb];
+	add		r10b, byte [stor + 0xb];
 	shl		r10, 8
-	add		r10, [stor + 0x7];
+	add		r10b, byte [stor + 0x7];
 	shl		r10, 8
-	add		r10, [stor + 0x3];
+	add		r10b, byte [stor + 0x3];
 
 	call 	shift
 	mov		[stor + 0x3], r10b
@@ -215,6 +231,245 @@ down:
 	leave
 	ret
 	
+up:
+	push 	rbp
+	mov 	rbp, rsp
+	sub 	rsp, 32
+
+	mov		r10b, byte [stor + 0x0];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x4];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x8];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xc];
+
+	call 	shift
+	mov		[stor + 0xc], r10b
+	shr		r10, 8
+	mov		[stor + 0x8], r10b
+	shr		r10, 8
+	mov		[stor + 0x4], r10b
+	shr		r10, 8
+	mov		[stor + 0x0], r10b
+	
+
+	mov		r10b, byte [stor + 0x1];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x5];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x9];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xd];
+
+	call 	shift
+	mov		[stor + 0xd], r10b
+	shr		r10, 8
+	mov		[stor + 0x9], r10b
+	shr		r10, 8
+	mov		[stor + 0x5], r10b
+	shr		r10, 8
+	mov		[stor + 0x1], r10b
+	
+
+	mov		r10b, byte [stor + 0x2];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x6];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xa];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xe];
+
+	call 	shift
+	mov		[stor + 0xe], r10b
+	shr		r10, 8
+	mov		[stor + 0xa], r10b
+	shr		r10, 8
+	mov		[stor + 0x6], r10b
+	shr		r10, 8
+	mov		[stor + 0x2], r10b
+	
+
+	mov		r10b, byte [stor + 0x3];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x7];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xb];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xf];
+
+	call 	shift
+	mov		[stor + 0xf], r10b
+	shr		r10, 8
+	mov		[stor + 0xb], r10b
+	shr		r10, 8
+	mov		[stor + 0x7], r10b
+	shr		r10, 8
+	mov		[stor + 0x3], r10b
+	
+	leave
+	ret
+
+
+left:
+	push 	rbp
+	mov 	rbp, rsp
+	sub 	rsp, 32
+
+	mov		r10b, byte [stor + 0x0];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x1];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x2];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x3];
+
+	call 	shift
+	mov		[stor + 0x3], r10b
+	shr		r10, 8
+	mov		[stor + 0x2], r10b
+	shr		r10, 8
+	mov		[stor + 0x1], r10b
+	shr		r10, 8
+	mov		[stor + 0x0], r10b
+	
+
+	mov		r10b, byte [stor + 0x4];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x5];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x6];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x7];
+
+	call 	shift
+	mov		[stor + 0x7], r10b
+	shr		r10, 8
+	mov		[stor + 0x6], r10b
+	shr		r10, 8
+	mov		[stor + 0x5], r10b
+	shr		r10, 8
+	mov		[stor + 0x4], r10b
+	
+
+	mov		r10b, byte [stor + 0x8];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x9];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xa];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xb];
+
+	call 	shift
+	mov		[stor + 0xb], r10b
+	shr		r10, 8
+	mov		[stor + 0xa], r10b
+	shr		r10, 8
+	mov		[stor + 0x9], r10b
+	shr		r10, 8
+	mov		[stor + 0x8], r10b
+	
+
+	mov		r10b, byte [stor + 0xc];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xd];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xe];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xf];
+
+	call 	shift
+	mov		[stor + 0xf], r10b
+	shr		r10, 8
+	mov		[stor + 0xe], r10b
+	shr		r10, 8
+	mov		[stor + 0xd], r10b
+	shr		r10, 8
+	mov		[stor + 0xc], r10b
+	
+	leave
+	ret
+
+right:
+	push 	rbp
+	mov 	rbp, rsp
+	sub 	rsp, 32
+
+	mov		r10b, byte [stor + 0x3];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x2];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x1];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x0];
+
+	call 	shift
+	mov		[stor + 0x0], r10b
+	shr		r10, 8
+	mov		[stor + 0x1], r10b
+	shr		r10, 8
+	mov		[stor + 0x2], r10b
+	shr		r10, 8
+	mov		[stor + 0x3], r10b
+	
+
+	mov		r10b, byte [stor + 0x7];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x6];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x5];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x4];
+
+	call 	shift
+	mov		[stor + 0x4], r10b
+	shr		r10, 8
+	mov		[stor + 0x5], r10b
+	shr		r10, 8
+	mov		[stor + 0x6], r10b
+	shr		r10, 8
+	mov		[stor + 0x7], r10b
+	
+
+	mov		r10b, byte [stor + 0xb];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xa];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x9];
+	shl		r10, 8
+	add		r10b, byte [stor + 0x8];
+
+	call 	shift
+	mov		[stor + 0x8], r10b
+	shr		r10, 8
+	mov		[stor + 0x9], r10b
+	shr		r10, 8
+	mov		[stor + 0xa], r10b
+	shr		r10, 8
+	mov		[stor + 0xb], r10b
+	
+
+	mov		r10b, byte [stor + 0xf];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xe];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xd];
+	shl		r10, 8
+	add		r10b, byte [stor + 0xc];
+
+	call 	shift
+	mov		[stor + 0xc], r10b
+	shr		r10, 8
+	mov		[stor + 0xd], r10b
+	shr		r10, 8
+	mov		[stor + 0xe], r10b
+	shr		r10, 8
+	mov		[stor + 0xf], r10b
+	
+	leave
+	ret
+
+
 shift:
 
 	push 	rbp
