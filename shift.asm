675d92aa85a710ab40e5589dedcef19c3ad9e335
Ivan Roganov <ivanr@scientology.net>
2021-05-26 19:28:54 -0700

    New algorythm

--- shift.asm
+++ shift.asm
@@ -2,88 +2,272 @@ shift:
 
 	push 	rbp
 	mov 	rbp, rsp
-	sub 	rsp, 16
-	;Shift happens only in one direction: From left to right. It's a matter of a viewpoint. 
-	;What you are trying to handle is just 4 numbers...
-	;	011f 001e 0011 0110 0ee1 1111 0000 1010 10e0 0101 010e 0001 1001 100e 1000
+	sub 	rsp, 32
 	
 	;1. If all 4 numbers are 0, return, we are done
-	;	011e 001e 0011 0110 0ee1 1111 xxxx 1010 10e0 0101 010e 0001 1001 100e 1000
-	test 	r10d, r10d
-	je		done
-
+	test 	r10, r10	;Is it zero? 
+	jne		shift_cont1	;If not, we are carry on
+	test 	r11, r11	;Check every register
+	jne		shift_cont1
+	test 	r12, r12
+	jne		shift_cont1
+	test 	r13, r13
+	jne		shift_cont1
+	jmp		done		;And if all of them are - wer are done here...
+shift_cont1:
 	;2. If first one is non zero and the rest is zero, do nothing...
-	;	011e 001e 0011 0110 0ee1 1111 xxxx 1010 10e0 0101 010e 0001 1001 100e xxxx
-	mov		r11d, r10d ;Check if we have only first digit
-	shl		r11d, 8
-	test	r11d, r11d
-	je		done
+	test	r10, r10
+	je		shift_cont2
+	test 	r11, r11
+	jne		shift_cont2
+	test 	r12, r12
+	jne		shift_cont2
+	test 	r13, r13
+	jne		shift_cont2
+	jmp		done
+shift_cont2:	;if the first 3 are zeroes
+	test	r10, r10
+	jne		shift_cont22
+	test 	r11, r11
+	jne		shift_cont22
+	test 	r12, r12
+	jne		shift_cont22
+	test 	r13, r13
+	je		shift_cont22
+	mov		r10, r13
+	mov		r13,0x0
+	jmp		done
+
+
+
+
+shift_cont22:
+
+	;if the first 2 and the last one is a zero, just move it and we are done
+	test	r10, r10
+	jne		shift_cont22222
+	test 	r11, r11
+	jne		shift_cont22222
+	test 	r12, r12
+	je		shift_cont22222
+	test 	r13, r13
+	jne		shift_cont22222
+	mov		r10, r12
+	mov		r12,0x0
+	jmp		done
+shift_cont22222:
+
+
+	;if the first 2 are zeroes
+	test	r10, r10
+	jne		shift_cont222
+	test 	r11, r11
+	jne		shift_cont222
+	test 	r12, r12
+	je		shift_cont222
+	test 	r13, r13
+	je		shift_cont222
+	mov		r10, r12
+	mov		r11, r13
+	mov		r12,0x0
+	mov		r13,0x0
+	jmp		solve_left_2
+shift_cont222:
+
+	;if the pattern is xoxo
+	test	r10, r10
+	je		shift_cont2222
+	test 	r11, r11
+	jne		shift_cont2222
+	test 	r12, r12
+	je		shift_cont2222
+	test 	r13, r13
+	jne		shift_cont2222
+	mov		r11, r12
+	mov		r12,0x0
+	jmp		solve_left_2
+shift_cont2222:
+
+	;Total rework of the algorythm. 
+	;We have 4 items.
+	;If the first or second are 0, remove it and solve the equation for first 3 items instead. 
+	;If the first one equals second one, add them up and solve the equation for the second 3 items.
+	;If the first one does not equal second one, solve the equation for the second 3 items. 
+	;xxxx => yxx_
+	;xxxo => yxo_
+	;xxoo => yoo_
+	;oxoo => xoo_
+	;xoxx => xxx_
+
+
+	;If the first or second are 0, remove it and solve the equation for first 3 items instead. 
+	test	r10, r10	;first one is zero
+	jne		shift_cont3
+	mov		r10, r11
+	mov 	r11, r12
+	mov		r12, r13
+	mov		r13, 0x0
 	
-	;3. If first byte is 0, Left Logical shift by 8 bits till it becomes something. 
-	;	11e0 1e00 1100 1100 ee10 1111 xxxx 1010 10e0 1010 10e0 1000 1001 100e xxxx
-	mov		r11d, r10d
-	shr		r11d, 24
-	test	r11d, r11d
-	jne 	doneshift
-	shl		r10d, 8 	;first symbol is not 0, so shift
+	jmp solve_first_3
 
-	mov		r11d, r10d
-	shr		r11d, 24
-	test	r11d, r11d
-	jne 	doneshift
-	shl		r10d, 8 	;second symbol is not 0, so shift
-
-	mov		r11d, r10d
-	shr		r11d, 24
-	test	r11d, r11d
-	jne 	doneshift
-	shl		r10d, 8 	;third symbol is not 0, so shift    
-doneshift:	;There can't be other sits in this scenario, so we are happy with what we have now, carry on.
+shift_cont3:
+	;If the first or second are 0, remove it and solve the equation for first 3 items instead. 
+	test	r11, r11	;second one is zero
+	jne		shift_cont33
+	mov 	r11, r12
+	mov		r12, r13
+	mov		r13, 0x0
 	
-	;4. Do the same for the bytes 2 and 3
-	;This is done by performing a rotaion and moving first byte to the end and checking leading zeroes.
-	;The amount of moves is stored in r12 and than un-does itself.
-	;	11e0 1e00 1100 1100 ee10 1111 xxxx 1100 1e00 1100 1e00 1000 1100 1e00 xxxx
-	; mov		r12, 0 ;How much to circularshift back if we want to.
-	; ror		r10w, 8	; moving the first digit to the end
-	; add		r12, 8
-	
-	; mov		r11w, r10w ;check if the first digit is zero
-	; shr		r11w, 24
-	; test	r11w, r11w
-	; je		done2shift
-	; shl		r10w, 8 	;first symbol is not 0, so shift
+	jmp solve_first_3
 
-	; ror		r10w, 8	; moving the first digit to the again
-	; add		r12, 8
-	
-	; mov		r11w, r10w ;check if the first digit is zero
-	; shr		r11w, 24
-	; test	r11w, r11w
-	; je		done2shift
-	; shl		r10w, 8 
+shift_cont33:
+	;If the first one equals second one, add them up and solve the equation for the second 3 items.
+	cmp		r10, r11	
+	jne		shift_cont4
+	inc		r10
+	mov		r11, r12
+	mov		r12, r13
+	mov		r13, 0x0
+	jmp solve_second_3
+shift_cont4:	
+	;If only the first and the last one are numbers, but the ones in the middle are empty
+	test	r11, r11
+	jne		shift_cont5
+	test	r12, r12
+	jne		shift_cont5
+	test	r13, r13
+	je		shift_cont5
+	cmp		r10, r13	;If they are equal, add them up
+	jne		shift_them
+	inc		r10
+	mov 	r13, 0x0
+	jmp		done
+shift_them:				;if not - put them together and you are done. 
+	mov 	r11, r13
+	mov		r13, 0x0
+	jmp		done
 
-; done2shift:
-; 	rol		r10w, r12	;Undo the damage of roling done on the previous step
-
-
-
-	; ;5. If there are only 1 number in the row, return it, no changes, basically re-doing step #2 after we had moved leading zeroes
-	; ;	11e0 1e00 1100 1100 ee10 1111 xxxx 1100 1e00 1100 1e00 xxxx 1100 1e00 xxxx
-	; mov		r11w, r10w ;Check if we have only first digit
-	; shl		r11w, 8
-	; test	r11w, r11w
-	; ret
-
-	;6. If the first two bytes are equal, inc first byte and remove the second byte
-	;	2e00 1e00 2000 2000 f100 2110 xxxx 2000 1e00 2000 1e00 xxxx 2000 1e00 xxxx
-
-	
-
-	;7. Repeat steps 1, 5, 6 byes 2 and 3
-	;	xxxx xxxx xxxx xxxx xxxx 2200 xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
-	;PROFIT!
-
+shift_cont5:
+	jmp solve_second_3
 done:
 	leave
 	ret
+
+	;solution for the first 3 and the second 3 is the same, just diferent registers are in use. 
+solve_first_3:
+	test 	r10, r10	;Is it zero? 
+	jne		solve_first_3_cont1	;If not, we are carry on
+	test 	r11, r11	;Check every register
+	jne		solve_first_3_cont1
+	test 	r12, r12
+	jne		solve_first_3_cont1
+	jmp		done
+
+solve_first_3_cont1:
+
+	test 	r10, r10	;If the first one is not zero, but the rest ones are zero
+	je		solve_first_3_cont2	;If not, we are carry on
+	test 	r11, r11	;Check every register
+	jne		solve_first_3_cont2
+	test 	r12, r12
+	jne		solve_first_3_cont2
+	jmp		done
+solve_first_3_cont2:
+
+	test	r11, r11	;If the middle one of them is zero, remove it and solve the first two 
+	jne		solve_first_3_cont3
+	mov		r11, r12
+	mov		r12, 0x0
+	jmp		solve_left_2
+solve_first_3_cont3:
+
+	test	r12, r12	;If the last one is zero, solve middle 2
+	jne		solve_first_3_cont4
+	jmp		solve_middle_2
+solve_first_3_cont4:
+	cmp		r10, r11
+	jne		solve_first_3_cont5
+	inc		r10
+	mov		r11, r12
+	mov		r12, 0x0
+solve_first_3_cont5:
+	cmp		r11, r12
+	jne		solve_first_3_cont6
+	inc		r11
+	mov		r12, 0x0
+solve_first_3_cont6:
+
+	jmp 	done
+
+
+
+solve_second_3: 
+	test 	r11, r11	;Is it zero? 
+	jne		solve_second_3_cont1	;If not, we are carry on
+	test 	r12, r12	;Check every register
+	jne		solve_second_3_cont1
+	test 	r13, r13
+	jne		solve_second_3_cont1
+	jmp		done
+solve_second_3_cont1:	
+
+	test 	r11, r11	;Is the first one is not zero, but the rest are zero
+	je		solve_second_3_cont2	;If not, we are carry on
+	test 	r12, r12	;Check every register
+	jne		solve_second_3_cont2
+	test 	r13, r13
+	jne		solve_second_3_cont2
+	jmp		done
+solve_second_3_cont2:	
+
+	test 	r11, r11	;If there is only one in the middle, remove zero and be done with it. 
+	jne		solve_second_3_cont22	;If not, we are carry on
+	test 	r12, r12	;Check every register
+	je		solve_second_3_cont22
+	test 	r13, r13
+	jne		solve_second_3_cont22
+	mov		r11, r12
+	mov		r12, 0x0
+	jmp		done
+solve_second_3_cont22:	
+
+	test	r12, r12	;If the middle one of them is zero, remove it and solve the first two 
+	jne		solve_second_3_cont3
+	mov		r12, r13
+	mov		r13, 0x0
+	jmp		solve_middle_2
+solve_second_3_cont3:
+	test	r13, r13	;If the last one is zero, solve middle 2
+	jne		solve_second_3_cont4
+	jmp		solve_middle_2
+
+;if all three of them are there:
+	cmp		r11, r12
+	jne	solve_second_3_cont4
+	inc		r11
+	mov		r12, r13
+	mov		r13, 0x0
+	jmp		done
+solve_second_3_cont4:
+
+	cmp		r12, r13
+	jne	solve_second_3_cont5
+	inc		r12
+	mov		r13, 0x0
+solve_second_3_cont5:
+
+	jmp 	done	
+
+solve_middle_2:
+	cmp		r11, r12
+	jne		done
+	inc		r11
+	mov		r12, 0x0
+	jmp		done
+
+solve_left_2:
+	cmp		r10, r11
+	jne		done
+	inc		r10
+	mov		r11, 0x0
+	jmp		done	
