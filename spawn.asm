02baa8a6c8b8c54ecf2113f98557bde325db2ffb
Ivan Roganov <ivanr@scientology.net>
2021-05-27 18:22:03 -0700

    Simple spawner

---
+++ spawn.asm
@@ -0,0 +1,24 @@
+spawn:  ;Simple spawner for now. Going thru the memory and just drop 1 in any free cell we can find. 
+	push 	rbp
+	mov 	rbp, rsp
+	sub 	rsp, 32
+
+    
+    mov     r14, 0
+spawn_loop:
+    mov     r15b, byte [stor+r14]
+
+    cmp     r15, 0x0
+    jne     spawn_continue
+    mov     byte [stor+r14], 1
+    jmp     spawn_done
+spawn_continue:
+    inc     r14
+    cmp     r14, 15
+    jne     spawn_loop
+    
+    call    loose
+    
+spawn_done:
+    leave
+	ret
