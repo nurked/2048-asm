7c4e79d84f8e1bebd2a97aaec4724c23a7b2cfd4
a.galimov <a.galimov@advengineering.ru>
2021-09-27 10:18:45 +0000

    add colors to output numbers

--- display.asm
+++ display.asm
@@ -4,9 +4,7 @@ showoff:
 
 	loop1:
 		mov	r13b, [stor + r14]
-		lea	rdx, [powers + r13 + r13*4]
-		mov	r8, 5
-		call	print
+		call	color_wrap
 		inc	r14
 		test	r14, 3
 		jnz	loop1
@@ -22,6 +20,23 @@ showoff:
 	lea	rdx, [newline]
 	mov	r8, 2
 	jmp	print ; tailcall
+
+color_wrap:
+	lea	rdx, [start_color]
+	mov	r8, 2
+	call	print
+	lea	rdx, [colors + r13 + r13*2]
+	mov	r8, 2
+	call	print
+	lea	rdx, [mid_color]
+	mov	r8, 1
+	call	print
+	lea	rdx, [powers + r13 + r13*4]
+	mov	r8, 5
+	call	print
+	lea	rdx, [end_color]
+	mov	r8, 4
+	call	print
 
 print: ; rdx->buffer r8->len
 	mov rcx, [hStdout]
